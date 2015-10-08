708e5a2c1f0e6bbf82a0e5a96ef3f52d4f65dd5c
Karen Hambro <khambro@gmail.com>
2015-10-08 14:43:43 -0700

    improves calculator

--- calc.rb
+++ calc.rb
@@ -2,18 +2,44 @@
 command = ""
 until command == "exit"
 
-puts "Hello, I'm a calculator, what would you like to do?"
+# number variables
+puts "Hello, I'm here for your basic mathematial calculation needs. What is the first number you would like to use?"
+
+num1 = gets.chomp
+
+num1=num1.to_i
+
+puts "What operator would you like to use?"
 
 command = gets.chomp
 
+until command == "add" || command == "+" || command ==  "subtract" || command == "-" || command == "multiply" || command == "*" || command == "divide" || command == "/"
+  puts "Please give me a valid operator (+,-,*,/). What operator would you like to use?"
+  command = gets.chomp
+  puts "Thank you."
+end
+
+  puts "What is your next number?"
+
+
+num2 = gets.chomp
+
+num2=num2.to_i
+
 if command == "add" || command== "+" || command== "plus"
-  puts "you want to add"
+  puts "you want to add #{num1} and #{num2}"
+  puts num1 + num2
 elsif command == "subtract" || command == "-"
-    puts "you want to subtract"
+    puts "you want to subtract #{num1} from #{num2}"
+    puts num1 - num2
 elsif command == "multiply" || command == "*"
-  puts "you want to multiply"
+  puts "you want to multiply #{num1} and #{num2}"
+  puts num1 * num2
+elsif command == "divide" || command == "/"
+  puts "you want to divide #{num1} and #{num2}"
+  puts "#{num1} / #{num2} = #{num1/num2}"
 else
-  puts "I don't know how to #{command}"
+  puts "I don't know how to #{command}. Please provide me with a basic equation you would like to calcualate."
 end
 
 end
