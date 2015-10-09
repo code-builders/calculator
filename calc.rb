0264b55be134fce8c9c41b5e72312d9f215e3fad
Karen Hambro <khambro@gmail.com>
2015-10-08 20:55:22 -0700

    Messes everything up

--- calc.rb
+++ calc.rb
@@ -4,21 +4,40 @@ while command != "exit".downcase
     puts "Hello, I'm a calculator, what would you like to do? To exit please write 'Exit'"
     command = gets.chomp.downcase
 
+
     if command == "add" || command == "+"
          puts "What's the first number you would like to add?"
          n1 = gets.chomp.to_i
+      else
+         puts "Please give me a valid operator (+,-,*,/). What operator would you like to use?"
+        command = gets.chomp.downcase
+        puts "What's the first number you would like to add?"
+        n1 = gets.chomp.to_i
+    end
+         #n1 = gets.chomp.to_i
          puts "What's the second number you would like to add?"
          n2 = gets.chomp.to_i
+      until (n1 != 0) || (n1.to_s != "0") && (n2 != 0) || (n2.to_s != "0")
+        puts "One or more of those numbers was not valid, please enter numbers only"
+        n1 = gets.chomp.to_i
+        n2 = gets.chomp.to_i
+      end
          puts "#{n1} + #{n2} = #{n1 + n2}"
-    end
+        
 
     if command == "subtract" || command == "-"
       puts "What's your first number?"
       n1 = gets.chomp.to_i
+    else
+      puts "Please give me a valid operator (+,-,*,/). What operator would you like to use?"
+      command = gets.chomp.downcase
+    end
+
+
       puts "What's your second number"
       n2 = gets.chomp.to_i
       puts "#{n1} - #{n2} = #{n1 - n2}"
-    end
+
 
     if command == "multiply" || command == "*" || command == "x"
       puts "What's the first number you would like to multiply?"
@@ -37,6 +56,6 @@ while command != "exit".downcase
     end
 
 
-
 end
+
 puts "I'm tired. Adios bitchachos."
